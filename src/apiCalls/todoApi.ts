b14cf7c9f7a28c9a222ed009081604df476c7ecd
uday kumar sah <web.uday@spaarksweb.com>
2024-04-02 14:58:19 +0530

    deployed backend at render

--- src/apiCalls/todoApi.ts
+++ src/apiCalls/todoApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todoApi = createApi({
   reducerPath: 'todoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }), // Update the baseUrl
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://todoapp-react-redux.onrender.com' }), // Update the baseUrl
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => 'todos',
