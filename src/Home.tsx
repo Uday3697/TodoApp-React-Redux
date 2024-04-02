d4936ec37cdc95324663214b3daa8879ebf0971b
uday kumar sah <web.uday@spaarksweb.com>
2024-04-02 19:05:03 +0530

    form integration assignmengt

---
+++ src/Home.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import TodoInput from "./components/TodoInput";
+import { LinearGradient } from "react-text-gradients";
+import { useAppSelector } from "./store/types";
+import "./App.css";
+
+
+const Home: React.FC = () => {
+  const alertMsgs = useAppSelector((state) => state.alert);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [currentAlertMsg, setCurrentAlertMsg] = useState<string>("");
+
+  useEffect(() => {
+    if (alertMsgs.length > 0) {
+      setCurrentAlertMsg(alertMsgs[0].message); // Assuming only one message is shown at a time
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+        setCurrentAlertMsg(""); // Clear the message after hiding the alert
+      }, 3000); // Show alert for 3 seconds
+    }
+  }, [alertMsgs]);
+
+  
+  return (
+      <div className="main-con">
+        <h1 style={{ textAlign: "center" }}>
+          <LinearGradient gradient={["to left", "#17acff ,#ff68f0"]}>
+            Fullstack ToDo App
+          </LinearGradient>
+        </h1>
+        <TodoInput />
+        {showAlert && <div className="alertmsg">{currentAlertMsg}</div>}
+      </div>
+     
+ 
+  );
+};
+
+
+export default Home
